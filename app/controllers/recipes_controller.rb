6e7db7285db19d61827e3ae29264b5eb3e65b2e5
darco <darek.charkot@gmail.com>
2015-09-16 06:32:07 +0200

    Prevent to create, update and delete a recipe before login user. Deleted layout application.html.erb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :set_recipe, :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   # GET /recipes
   # GET /recipes.json
