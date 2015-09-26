df663887336079abe8e6ec08de3778e2bd8fe93f
darco <darek.charkot@gmail.com>
2015-09-26 14:59:41 +0200

    Add column makerecipe to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812184035) do
+ActiveRecord::Schema.define(version: 20150926125754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150812184035) do
     t.text     "ingredients"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "makerecipe"
   end
 
   create_table "users", force: :cascade do |t|
