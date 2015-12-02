0c1a72c2c8ab49141524416cbfde434a4dcb8f84
darco <darek.charkot@gmail.com>
2015-12-02 07:37:26 +0100

    Add comments about recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @comments = Comment.where(recipe_id: params[:id])
   end
 
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101094254) do
+ActiveRecord::Schema.define(version: 20151123205514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string  "name"
+    t.string  "text"
+    t.integer "recipe_id"
+  end
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
